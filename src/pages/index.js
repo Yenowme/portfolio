7d2130add668dccce75a8265a0a002db886a1358
Yenowme <76904042+Yenowme@users.noreply.github.com>
2022-02-15 18:56:47 +0900

    feat: 스티커 추가, 위치조절 필요 #4

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,6 +14,71 @@ import gitHubHoverImg from "../images/githubSticker_hover.png"
 import NameNormalImg from "../images/NameSticker_normal.png"
 import NameHoverImg from "../images/NameSticker_hover.png"
 import NameClickImg from "../images/NameSticker_click.png"
+import starNormalImg from "../images/StarSticker_normal.png"
+import starHoverImg from "../images/StarSticker_hover.png"
+import starClickImg from "../images/StarSticker_click.png"
+import start2NormalImg from "../images/StarSticker_s_normal.png"
+import start2HoverImg from "../images/StarSticker_s_hover.png"
+
+const Stickers = () => (
+  <>
+    <Sticker
+      normalImg={gitHubNormalImg}
+      hoverImg={gitHubHoverImg}
+      clickImg={gitHubClickImg}
+      alt={"gitHub"}
+      width={"140"}
+      height={"140"}
+      url={"https://github.com/Yenowme"}
+      positionTop={"50%"}
+      positionLeft={"76%"}
+      rotate={""}
+    />
+    <Sticker
+      normalImg={NameNormalImg}
+      hoverImg={NameHoverImg}
+      clickImg={NameClickImg}
+      alt={"name"}
+      width="385"
+      height={"138"}
+      positionTop={"65%"}
+      positionLeft={"75%"}
+      rotate={"-18"}
+    />
+    <Sticker
+      normalImg={start2NormalImg}
+      hoverImg={start2HoverImg}
+      clickImg={start2HoverImg}
+      alt={"start2"}
+      width="135"
+      height={"135"}
+      positionLeft={"65%"}
+      positionTop={"62%"}
+      rotate="-15"
+    />
+    <Sticker
+      normalImg={start2NormalImg}
+      hoverImg={start2HoverImg}
+      clickImg={start2HoverImg}
+      alt={"start2"}
+      width="100"
+      height={"100"}
+      positionLeft={"10px"}
+      positionTop={"49%"}
+      rotate="-10"
+    />
+    <Sticker
+      normalImg={starNormalImg}
+      hoverImg={starHoverImg}
+      clickImg={starClickImg}
+      alt={"start1"}
+      width="135"
+      height={"135"}
+      positionLeft={"1030px"}
+      positionTop={"28%"}
+    />
+  </>
+)
 
 const Landing = () => (
   <>
@@ -42,29 +107,7 @@ const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Landing />
-    <Sticker
-      normalImg={gitHubNormalImg}
-      hoverImg={gitHubHoverImg}
-      clickImg={gitHubClickImg}
-      alt={"gitHub"}
-      width={"140"}
-      height={"140"}
-      url={"https://github.com/Yenowme"}
-      positionTop={"50%"}
-      positionLeft={"80%"}
-      rotate={""}
-    />
-    <Sticker
-      normalImg={NameNormalImg}
-      hoverImg={NameHoverImg}
-      clickImg={NameClickImg}
-      alt={"name"}
-      width="385"
-      height={"138"}
-      positionTop={"70%"}
-      positionLeft={"60%"}
-      rotate={"-18"}
-    />
+    <Stickers />
     <Divider title="PROJECTS" />
     <div className={`${Style.gradientOrange}`}></div>
     <div className={`${Style.gradientYellow}`}></div>
