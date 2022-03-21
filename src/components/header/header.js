af615d5e0e662b99f24657fef0baa8629cf7bdf2
Yenowme <76904042+Yenowme@users.noreply.github.com>
2022-03-21 20:54:24 +0900

    feat: Navbar 클릭시 이동, 호버 스타일 추가 #5

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,23 +2,43 @@ import * as React from "react"
 import { Link } from "gatsby"
 import * as headerStyle from "./header.module.css"
 
-const Header = () => (
-  <header className={headerStyle.head}>
-    <Link className={headerStyle.nav} to="/">
-      portfolio
-    </Link>
-    <div>
-      <Link className={`${headerStyle.nav} ${headerStyle.right}`} to="/">
-        about
-      </Link>
-      <Link className={`${headerStyle.nav} ${headerStyle.right}`} to="/">
-        projects
-      </Link>
-      <Link className={`${headerStyle.nav} ${headerStyle.right}`} to="/">
-        contact
-      </Link>
-    </div>
-  </header>
-)
-
+const Header = () => {
+  const onClick = top => {
+    window.scrollTo({
+      top: top,
+      behavior: "smooth",
+    })
+  }
+  return (
+    <header className={headerStyle.head}>
+      <button
+        onClick={() => {
+          onClick(0)
+        }}
+        className={headerStyle.nav}
+        to="/"
+      >
+        portfolio
+      </button>
+      <div>
+        <button
+          className={`${headerStyle.nav} ${headerStyle.right}`}
+          onClick={() => {
+            onClick(document.body.offsetHeight)
+          }}
+        >
+          projects
+        </button>
+        <button
+          onClick={() => {
+            onClick(document.body.scrollHeight)
+          }}
+          className={`${headerStyle.nav} ${headerStyle.right}`}
+        >
+          contact
+        </button>
+      </div>
+    </header>
+  )
+}
 export default Header
